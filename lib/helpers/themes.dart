0983c2f4a93f1f3192c2458f2b83394d573a0370
fontseca <alx.17@outlook.com>
2022-01-27 08:43:05 -0600

    feat: add assets, themes and fonts

---
+++ lib/helpers/themes.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:analyseur/static/colors.dart';
+
+AppBarTheme appBarTheme = AppBarTheme(
+  color: Colors.transparent,
+  elevation: 0
+);
+
+ThemeData lightTheme(BuildContext context) {
+  return ThemeData(
+    appBarTheme: appBarTheme,
+    primaryColor: kPrimaryColor,
+    accentColor: kAccentLightColor,
+    scaffoldBackgroundColor: Colors.white,
+    backgroundColor: Colors.white,
+    colorScheme: ColorScheme.light(secondary: kSecondaryLightColor),
+    iconTheme: IconThemeData(color: kBodyTextColorLight),
+    accentIconTheme: IconThemeData(color: kAccentIconLightColor),
+    primaryIconTheme: IconThemeData(color: kPrimaryIconLightColor),
+    textTheme: GoogleFonts.montserratTextTheme().copyWith(
+      bodyText1: TextStyle(color: kBodyTextColorLight),
+      bodyText2: TextStyle(color: kBodyTextColorLight),
+      headline4: TextStyle(color: kTitleTextLightColor, fontSize: 32),
+      headline1: TextStyle(color: kTitleTextLightColor, fontSize: 80),
+    )
+  );
+}
+
+ThemeData darkTheme(BuildContext context) {
+  return ThemeData.dark().copyWith(
+    primaryColor: kPrimaryColor,
+    accentColor: kAccentDarkColor,
+    scaffoldBackgroundColor: Color(0xFF0D0C0E),
+    appBarTheme: appBarTheme,
+    colorScheme: ColorScheme.light(
+      secondary: kSecondaryDarkColor,
+      surface: kSurfaceDarkColor,
+    ),
+    backgroundColor: kBackgroundDarkColor,
+    iconTheme: IconThemeData(color: kBodyTextColorDark),
+    accentIconTheme: IconThemeData(color: kAccentIconDarkColor),
+    primaryIconTheme: IconThemeData(color: kPrimaryIconDarkColor),
+    textTheme: GoogleFonts.montserratTextTheme().copyWith(
+      bodyText1: TextStyle(color: kBodyTextColorDark),
+      bodyText2: TextStyle(color: kBodyTextColorDark),
+      headline4: TextStyle(color: kTitleTextDarkColor, fontSize: 32),
+      headline1: TextStyle(color: kTitleTextDarkColor, fontSize: 80),
+    )
+  );
+}
