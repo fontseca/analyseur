78ad8ff40f2c42bc7ed81f3f15b2b179cd6cf126
fontseca <alx.17@outlook.com>
2022-01-27 09:50:20 -0600

    feat: add activities screen

---
+++ lib/screens/activities/activities.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:analyseur/helpers/sizes.dart';
+
+class ActivitiesScreen extends StatefulWidget {
+  ActivitiesScreen({Key? key}) : super(key: key);
+
+  @override
+  _ActivitiesScreenState createState() => _ActivitiesScreenState();
+}
+
+class _ActivitiesScreenState extends State<ActivitiesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Scaffold(
+      appBar: buildAppBar(context),
+      body: Center(
+        child: Text('Welcome to Analyseur')
+      )
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      title: Text('Activities'),
+      leading: IconButton(
+        icon: SvgPicture.asset(
+          'assets/icons/settings.svg',
+          color: Theme.of(context).iconTheme.color,
+        ),
+        onPressed: () {},
+      ),
+      actions: [buildAddButton(context)],
+    );
+  }
+
+  Padding buildAddButton(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: getProportionateScreenWidth(10),
+      ),
+      child: InkWell(
+        onTap: () {},
+        child: Container(
+          width: getProportionateScreenWidth(32),
+          decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              shape: BoxShape.circle),
+          child: Icon(Icons.add, color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
