854399c45a59d7d753b56c61682253324748a512
fontseca <alx.17@outlook.com>
2022-02-04 20:42:52 -0600

    feat: activity can be logged

--- lib/screens/activities/widgets/activity.dart
+++ lib/screens/activities/widgets/activity.dart
@@ -1,79 +1,101 @@
+import 'package:analyseur/classes/activity/record/record.dart';
 import 'package:analyseur/helpers/sizes.dart';
-import 'package:analyseur/static/colors.dart';
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class Activity extends StatelessWidget {
   final String activityName;
   final Color activityColor;
+  late String desc;
+  late double total;
+  late List<Record> activityRecords = [];
+  late Record record;
 
-  const Activity({required this.activityName, required this.activityColor});
+  Activity({required this.activityName, required this.activityColor});
+
+  void startActivity() {
+    bool recordExists = false;
+    late Record existingRecord;
+
+    // if there are records
+    if (activityRecords.isNotEmpty) {
+      // look for current day's record
+      activityRecords.forEach((record) {
+        if (record.id == record.generateId()) {
+          recordExists = true;
+          existingRecord = record;
+        }
+      });
+    }
+
+    // if there are not records for the current day
+    if (!recordExists) {
+      // then create a new record
+      record = new Record(this.activityName);
+      record.id = record.generateId();
+      print('Created \'${record.id}\' record');
+      // and start the logging the time of the activity
+      print('Starting log for \'${record.id}\'');
+      record.startLog();
+      activityRecords.add(record);
+    } else {
+      print('Starting log for \'${existingRecord.id}\'');
+      existingRecord.startLog();
+    }
+  }
+
+  void finishAcitivity() {
+    print('Finished log of \'${record.id}\'');
+    record.finishLog();
+  }
+
+  void printactivityRecords() {
+    if (activityRecords.isEmpty) {
+      print('NO ACTIVITY RECORDS YET\n');
+      return;
+    }
+    activityRecords.forEach((record) {
+      print('PRINTING ACTIVITY RECORDS\n----------------');
+      print('Total duration of \'${record.id}\': ${record.totalDuration}');
+      print('Logs of \'${record.id}\'');
+      record.recordLogs.forEach((rl) {
+        print('\t ${rl.duration()} seconds');
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding:
-          EdgeInsets.symmetric(vertical: getProportionateScreenHeight(10)),
-            child: Container(
-              height: getProportionateScreenHeight(80),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(new Radius.circular(5.0)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.10),
-                    spreadRadius: 1,
-                    blurRadius: 10,
-                    offset: Offset(0, 2),
-                  ),
-                ],
-                border: Border.all(color: Color(0xFF242A38).withOpacity(0.10), width: 1),
-                color: Colors.white,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Leading(
-                    activityName: this.activityName,
-                    activityColor: this.activityColor,
-                  ),
-                  Actions()
-            ],
-          ),
+      padding: EdgeInsets.symmetric(vertical: getProportionateScreenHeight(10)),
+      child: Container(
+        height: getProportionateScreenHeight(80),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(new Radius.circular(5.0)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.10),
+              spreadRadius: 1,
+              blurRadius: 10,
+              offset: Offset(0, 2),
+            ),
+          ],
+          border:
+              Border.all(color: Color(0xFF242A38).withOpacity(0.10), width: 1),
+          color: Theme.of(context).backgroundColor,
         ),
-      );
-  }
-}
-
-class Actions extends StatelessWidget {
-  const Actions({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        MaterialButton(
-          minWidth: 10,
-          height: 40,
-          visualDensity: VisualDensity.compact,
-          shape: const CircleBorder(
-              side: BorderSide(color: kBodyTextColorLight, width: 1)),
-          onPressed: () => {},
-          child: const Icon(
-            Icons.arrow_drop_down,
-          ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Leading(
+              this,
+              activityName: this.activityName,
+              activityColor: this.activityColor,
+            ),
+            Actions(this),
+          ],
         ),
-        MaterialButton(
-          color: kBodyTextColorLight,
-          minWidth: 10,
-          height: 40,
-          shape: const CircleBorder(
-              side: BorderSide(color: kBodyTextColorLight, width: 1)),
-          onPressed: () => {},
-          child: const Icon(
-            Icons.play_arrow,
-            color: Colors.white,
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
@@ -81,41 +103,104 @@ class Actions extends StatelessWidget {
 class Leading extends StatelessWidget {
   final String activityName;
   final Color activityColor;
-  const Leading({required this.activityName, required this.activityColor});
+  final Activity self;
+
+  const Leading(this.self,
+      {required this.activityName, required this.activityColor});
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
+        // Settings
         MaterialButton(
           minWidth: 10,
           height: 40,
           visualDensity: VisualDensity.compact,
           shape: const CircleBorder(),
-          onPressed: () => {},
-          child: Icon(Icons.settings),
+          onPressed: () {
+            // Settings
+            // print('Setting up ${this.activityName}');
+            self.printactivityRecords();
+          },
+          child: Icon(
+            Icons.settings,
+            color: Theme.of(context).iconTheme.color,
+          ),
         ),
         Container(
           width: 40,
           height: 40,
           margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
           decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(new Radius.circular(50.0)),
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.black.withOpacity(0.10),
-                    spreadRadius: 0,
-                    blurRadius: 4,
-                    offset: Offset(0, 2)),
-              ],
-              color: this.activityColor),
+            borderRadius: BorderRadius.all(new Radius.circular(50.0)),
+            color: this.activityColor,
+          ),
         ),
         Text(
           this.activityName,
-          style: TextStyle(
-            fontWeight: FontWeight.bold
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+}
+
+class Actions extends StatefulWidget {
+  final Activity self;
+
+  Actions(this.self);
+
+  @override
+  _ActionsState createState() => _ActionsState();
+}
+
+class _ActionsState extends State<Actions> {
+  bool _isPlaying = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        MaterialButton(
+          color: Theme.of(context).iconTheme.color,
+          minWidth: 10,
+          height: 40,
+          visualDensity: VisualDensity.compact,
+          shape: CircleBorder(),
+          child: Icon(
+            _isPlaying ? Icons.stop : Icons.play_arrow,
+            color: _isPlaying ? widget.self.activityColor : Theme.of(context).accentIconTheme.color,
           ),
-        )
+          onPressed: () {
+            if (!_isPlaying) {
+              widget.self.startActivity();
+            } else {
+              widget.self.finishAcitivity();
+            }
+            setState(() {
+              _isPlaying = !_isPlaying;
+            });
+          },
+        ),
+        // Show dashboard
+        MaterialButton(
+          minWidth: 10,
+          height: 35,
+          visualDensity: VisualDensity.compact,
+          shape: CircleBorder(),
+          child: Icon(
+            Icons.chevron_right,
+            color: Theme.of(context).iconTheme.color,
+            size: 35,
+          ),
+          onPressed: () {
+            // self.startActivity();
+            print('Show dashboard');
+            // self.printactivityRecords();
+            // widget.self.finishAcitivity();
+          },
+        ),
       ],
     );
   }
