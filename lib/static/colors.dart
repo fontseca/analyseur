02694e62bdd2ca4ccb175db21ec115f993295923
fontseca <alx.17@outlook.com>
2022-02-04 18:00:26 -0600

    refactor: change color

--- lib/static/colors.dart
+++ lib/static/colors.dart
@@ -4,11 +4,11 @@ const kPrimaryColor           = Color(0xFFFF97B3);
 const kSecondaryLightColor    = Color(0xFFE4E9F2);
 const kSecondaryDarkColor     = Color(0xFF404040);
 const kAccentLightColor       = Color(0xFFFFFFFF);
-const kAccentDarkColor        = Color(0xFF4E4E4E);
+const kAccentDarkColor        = Color(0xFF151515);
 const kBackgroundDarkColor    = Color(0xFF3A3A3A);
 const kSurfaceDarkColor       = Color(0xFF222225);
 
-const kAccentIconLightColor   = Color(0xFF242A38);
+const kAccentIconLightColor   = Color(0xFFFFFFFF);
 const kAccentIconDarkColor    = Color(0xFF303030);
 const kPrimaryIconLightColor  = Color(0xFF242A38);
 const kPrimaryIconDarkColor   = Color(0xFF232323);
