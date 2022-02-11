5ce366676465c83f1aaee75f0d544b34b512b475
fontseca <alx.17@outlook.com>
2022-02-11 08:15:52 -0600

    feat: remove clock

--- lib/screens/activities/widgets/body.dart
+++ lib/screens/activities/widgets/body.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-import './clock.dart';
 import './activies-list.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [DigitalClockAndDate(), Activities()],
-    );
-    // return Column(children: [DigitalClockAndDate(), Activities()]);
+    return Activities();
   }
 }
